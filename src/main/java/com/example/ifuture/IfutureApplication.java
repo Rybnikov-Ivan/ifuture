1673469521290702312a8bd4b17f071a6e580169
iva <rybnikovv12345@gmail.com>
2022-09-08 12:36:01 +0400

    Initial commit

---
+++ src/main/java/com/example/ifuture/IfutureApplication.java
@@ -0,0 +1,13 @@
+package com.example.ifuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ifuture/IfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ifuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
